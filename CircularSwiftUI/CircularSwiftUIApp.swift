b06ebe1f7d0da0a7db5983757262fdb2a942cda0
Onur Bulut <10urbulut@gmail.com>
2023-10-05 08:04:04 +0300

    first commit

---
+++ CircularSwiftUI/CircularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSwiftUIApp.swift
+//  CircularSwiftUI
+//
+//  Created by Onur Bulut on 4.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
